63c15a7d4e72597e0695b406091f83dc666b2556
Chad Selph <chad.selph@gmail.com>
2017-04-11 16:58:51 +0300

    only need pyobjc-framework-Cocoa, not whole pyobjc

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     package_data={'': ['LICENSE']},
     long_description=readme + '\n\n' + changes,
     license='BSD License',
-    install_requires=['pyobjc'],
+    install_requires=['pyobjc-framework-Cocoa'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
