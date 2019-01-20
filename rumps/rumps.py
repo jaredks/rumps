d44b5f753fa018eade6942a7146d0143a7369e43
Vladimir Berkutov <vladimirb@indeed.com>
2019-01-20 21:51:39 +0900

    Remove unnecessary class

--- rumps/rumps.py
+++ rumps/rumps.py
@@ -283,41 +283,6 @@ def timer(interval):
     return decorator
 
 
-class _ClickedProxy(object):
-    """
-    Replaces a function or a method decorated with :func:`clicked`
-    and allows both transparent function calls as well as access to :class:`rumps.MenuItem` instance
-    through :meth:`rumps._ClickedProxy.menuitem`.
-
-    See `examples/example_init.py` for usage.
-    """
-    def __init__(self, fn):
-        """
-        Proxy that transparently
-        :param fn:
-        """
-        self._fn = fn
-        self._menuitem = None
-
-    @property
-    def menuitem(self):
-        """
-        :returns: :class:`MenuItem` instance, or `None` in case of proxy has not yet been initialized
-        """
-        return self._menuitem
-
-    @menuitem.setter
-    def menuitem(self, value):
-        """
-        Sets MenuItem instance.
-        :param value: :class:`MenuItem` instance
-        """
-        self._menuitem = value
-
-    def __call__(self, *args, **kwargs):
-        return self._fn(*args, **kwargs)
-
-
 def clicked(*args, **options):
     """Decorator for registering a function as a callback for a click action on a :class:`rumps.MenuItem` within the
     application. The passed `args` must specify an existing path in the main menu. The :class:`rumps.MenuItem`
@@ -337,10 +302,8 @@ def clicked(*args, **options):
     :param args: a series of strings representing the path to a :class:`rumps.MenuItem` in the main menu of the
                  application.
     :param key: a string representing the key shortcut as an alternative means of clicking the menu item.
-    :returns: A :class:`rumps._ClickedProxy` that allows to access menu items
     """
     def decorator(f):
-        callback_proxy = wraps(f)(_ClickedProxy(f))
 
         def register_click(self):
             menuitem = self._menu  # self not defined yet but will be later in 'run' method
@@ -353,13 +316,13 @@ def clicked(*args, **options):
                     menuitem.add(arg)
                     menuitem = menuitem[arg]
             menuitem.set_callback(f, options.get('key'))
-            callback_proxy.menuitem = menuitem
+            setattr(f, 'menuitem', menuitem)
 
         # delay registering the button until we have a current instance to be able to traverse the menu
         buttons = clicked.__dict__.setdefault('*buttons', [])
         buttons.append(register_click)
 
-        return callback_proxy
+        return f
     return decorator
 
 
